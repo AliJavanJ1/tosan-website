7bdaa8ceb7e136ba35ab2d821a5c21757253ba93
moeiniamir <amirmoini@outlook.com>
2022-07-14 22:51:28 +0430

    add rtl

--- tosan_front/src/App.js
+++ tosan_front/src/App.js
@@ -5,9 +5,20 @@ import {useSelector} from "react-redux";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import {red} from "@mui/material/colors";
 import {Typography} from "@mui/material";
+import {Header} from "./components/header";
+import {CacheProvider} from "@emotion/react";
+import {prefixer} from "stylis";
+import rtlPlugin from 'stylis-plugin-rtl';
+import createCache from '@emotion/cache';
 
+
+const cacheRtl = createCache({
+    key: 'muirtl',
+    stylisPlugins: [prefixer, rtlPlugin],
+});
 
 const theme = createTheme({
+    direction: 'rtl',
     palette: {
         primary: {
             main: red[500],
@@ -51,17 +62,20 @@ const theme = createTheme({
 });
 
 function App() {
-    // const temp = useSelector((state)=> state.app)
+    const temp = useSelector((state)=> state.app)
     // console.log(temp)
 
   return (
-      <ThemeProvider theme={theme}>
-          <BrowserRouter>
-              <Routes>
-                  <Route path={'/'} element={<Typography></Typography>}/>
-              </Routes>
-          </BrowserRouter>
-      </ThemeProvider>
+      <CacheProvider value={cacheRtl}>
+          <ThemeProvider theme={theme}>
+              <BrowserRouter>
+                  <Header/>
+                  <Routes>
+                      <Route path={'/'} element={<Typography></Typography>}/>
+                  </Routes>
+              </BrowserRouter>
+          </ThemeProvider>
+      </CacheProvider>
   );
 }
 
