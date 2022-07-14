4da09201d909843ebf1a3d713efc07021b31130b
AJAliJavan <aj.ali.javan@gmail.com>
2022-07-14 21:27:29 +0430

    feat(front): add theme and font to App.js

--- tosan_front/src/App.js
+++ tosan_front/src/App.js
@@ -1,14 +1,63 @@
 import {
     BrowserRouter, Route, Routes
 } from "react-router-dom";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import {red} from "@mui/material/colors";
+import {Typography} from "@mui/material";
+
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: red[500],
+        },
+    },
+    typography: {
+        fontFamily: 'IRANSansXRegular',
+        black: {
+            fontFamily: 'IRANSansXBlack',
+        },
+        bold: {
+            fontFamily: 'IRANSansXBold',
+        },
+        demiBold: {
+            fontFamily: 'IRANSansXDemiBold',
+        },
+        extraBold: {
+            fontFamily: 'IRANSansXExtraBold',
+        },
+        light: {
+            fontFamily: 'IRANSansXLight',
+        },
+        medium: {
+            fontFamily: 'IRANSansXMedium',
+        },
+        regular: {
+            fontFamily: 'IRANSansXRegular',
+        },
+        thin:{
+            fontFamily: 'IRANSansXThin',
+        },
+        ultraLight: {
+            fontFamily: 'IRANSansXUltraLight',
+        },
+    },
+    components: {
+        MuiCssBaseline: {
+
+        },
+    },
+});
 
 function App() {
   return (
-      <BrowserRouter>
-          <Routes>
-              <Route path={'/'} element={<>hi</>}/>
-          </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+          <BrowserRouter>
+              <Routes>
+                  <Route path={'/'} element={<Typography></Typography>}/>
+              </Routes>
+          </BrowserRouter>
+      </ThemeProvider>
   );
 }
 
