b12c0c003ceebf23b028bc461918523a27b76675
AJAliJavan <aj.ali.javan@gmail.com>
2022-07-17 22:42:59 +0430

    feat(front/src/components/Phone): get phone number from server

--- tosan_front/src/components/Phone.js
+++ tosan_front/src/components/Phone.js
@@ -1,11 +1,17 @@
 import {Grid, Link, Typography} from "@mui/material";
 import {toFarsiNumber} from "../utils";
+import {useSelector} from "react-redux";
 
-function Phone({number, color}) {
-    const pre_number = number.slice(0, 3)
-    const main_number = number.slice(3)
+function Phone({color}) {
+    const number = useSelector(store => (store.app && store.app.general_data.phone_number.value))
+    let pre_number = null
+    let main_number = null
+    if (number) {
+        pre_number = number.slice(0, 3)
+        main_number = number.slice(3)
+    }
     return (
-        <Link href={"call/" + number} underline="none">
+        number && <Link href={"call/" + number} underline="none">
             <Grid
                 container
                 direction="row"
