4fc0ba42046522fa454df0e0b799b1bf5b84bb39
moeiniamir <amirmoini@outlook.com>
2022-07-29 23:26:31 +0430

    feat(filterSlice): add checkbox filter state

--- tosan_front/src/redux/filterSlice.js
+++ tosan_front/src/redux/filterSlice.js
@@ -1,18 +1,40 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit'
 
 const initialState = {
     'quickFilterInput': '',
+    'checkBoxFilter': {
+        'split': [],
+    },
 }
 
 const filterSlice = createSlice({
     name: 'filter',
     initialState,
     reducers: {
-        setQuickFilterInput(state, action){
+        setQuickFilterInput(state, action) {
             state.quickFilterInput = action.payload
+        },
+        setSplitFilterItemState: {
+            reducer: (state, action) => {
+                if (action.payload.add) {
+                    state.checkBoxFilter[action.payload.name].push(action.payload.value)
+                } else {
+                    state.checkBoxFilter[action.payload.name] = state.checkBoxFilter[action.payload.name]
+                        .filter(item => item !== action.payload.value)
+                }
+            },
+            prepare: (name, value, add) => {
+                return {
+                    payload: {
+                        name,
+                        value,
+                        add,
+                    }
+                }
+            }
         },
     },
 })
 
-export const {setQuickFilterInput} = filterSlice.actions
+export const {setQuickFilterInput, setSplitFilterItemState} = filterSlice.actions
 export default filterSlice.reducer
