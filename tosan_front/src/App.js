68048157bb0026f0a245bea4406ebe91d0c85677
moeiniamir <amirmoini@outlook.com>
2022-07-29 18:58:03 +0430

    install x-data-grid-pro

--- tosan_front/src/App.js
+++ tosan_front/src/App.js
@@ -14,10 +14,10 @@ import PhoneCall from "./components/PhoneCall";
 import Redirect from "./components/Redirect";
 import Footer from "./components/Footer";
 import MainPage from "./pages/MainPage";
-import MainSlider from "./components/MainSlider";
-import ProductsSlideshow from "./components/productsSlideshow";
 import Header from "./components/header";
+import {LicenseInfo} from '@mui/x-license-pro';
 
+LicenseInfo.setLicenseKey('x0jTPl0USVkVZV0SsMjM1kDNyADM5cjM2ETPZJVSQhVRsIDN0YTM6IVREJ1T0b9586ef25c9853decfa7709eee27a1e');
 
 const cacheRtl = createCache({
     key: 'muirtl',
@@ -25,119 +25,120 @@ const cacheRtl = createCache({
 });
 
 const theme = createTheme({
-    direction: 'rtl',
-    palette: {
-        primary: {
-            main: `#7f8d9e`,
-            shade1: `#7f8d9e`,
-            // shade2: ,
-            shade3: `#334964`,
-            shade4: `#001b3e`,
+        direction: 'rtl',
+        palette: {
+            primary: {
+                main: `#7f8d9e`,
+                shade1: `#7f8d9e`,
+                // shade2: ,
+                shade3: `#334964`,
+                shade4: `#001b3e`,
+            },
+            secondary: {
+                main: `#D8CE9D`,
+                shade1: `#D8CE9D`,
+                shade2: `#D3C68B`,
+                shade3: `#CDBE78`,
+            },
+            grey: {
+                main: `#383838`,
+                shade1: `#c2c7ce`,
+                shade2: `#c4c4c4`,
+                shade3: `#888888`,
+                shade4: `#383838`,
+                shade5: `#000000`,
+            },
+            white: {
+                main: `#f2f2f2`,
+                shade1: `#f2f2f2`,
+                shade2: `#f7f7f7`,
+                shade3: `#ffffff`,
+            }
         },
-        secondary: {
-            main: `#D8CE9D`,
-            shade1: `#D8CE9D`,
-            shade2: `#D3C68B`,
-            shade3: `#CDBE78`,
-        },
-        grey: {
-            main: `#383838`,
-            shade1: `#c2c7ce`,
-            shade2: `#c4c4c4`,
-            shade3: `#888888`,
-            shade4: `#383838`,
-            shade5: `#000000`,
-        },
-        white: {
-            main: `#f2f2f2`,
-            shade1: `#f2f2f2`,
-            shade2: `#f7f7f7`,
-            shade3: `#ffffff`,
-        }
-    },
-    typography: {
-        fontFamily: 'IRANSansXRegular',
-        fontSize: 14,
-        extraBoldX:{
-            fontFamily: 'IRANSansXExtraBold',
-            fontSize: 36,
-            lineHeight: "54px",
-        },
-        extraBold: {
-            fontFamily: 'IRANSansXExtraBold',
-            fontSize: 28,
-            lineHeight: "42px",
-        },
-        bold: {
-            fontFamily: 'IRANSansXBold',
-            fontSize: 24,
-            lineHeight: "36px",
-        },
-        boldS: {
-            fontFamily: 'IRANSansXBold',
-            fontSize: 18,
-            lineHeight: "27px",
-        },
-        demiBoldX: {
-            fontFamily: 'IRANSansXDemiBold',
-            fontSize: 18,
-            lineHeight: "27px",
+        typography: {
+            fontFamily: 'IRANSansXRegular',
+            fontSize: 14,
+            extraBoldX: {
+                fontFamily: 'IRANSansXExtraBold',
+                fontSize: 36,
+                lineHeight: "54px",
+            },
+            extraBold: {
+                fontFamily: 'IRANSansXExtraBold',
+                fontSize: 28,
+                lineHeight: "42px",
+            },
+            bold: {
+                fontFamily: 'IRANSansXBold',
+                fontSize: 24,
+                lineHeight: "36px",
+            },
+            boldS: {
+                fontFamily: 'IRANSansXBold',
+                fontSize: 18,
+                lineHeight: "27px",
+            },
+            demiBoldX: {
+                fontFamily: 'IRANSansXDemiBold',
+                fontSize: 18,
+                lineHeight: "27px",
 
+            },
+            demiBold: {
+                fontFamily: 'IRANSansXDemiBold',
+                fontSize: 16,
+                lineHeight: "24px",
+            },
+            demiBoldS: {
+                fontFamily: 'IRANSansXDemiBold',
+                fontSize: 14,
+                lineHeight: "21px",
+            },
+            mediumS: {
+                fontFamily: 'IRANSansXMedium',
+                fontSize: 14,
+                lineHeight: "21px",
+            },
+            medium: {
+                fontFamily: 'IRANSansXMedium',
+                fontSize: 16,
+                lineHeight: "24px",
+            },
+            mediumX: {
+                fontFamily: 'IRANSansXMedium',
+                fontSize: 16,
+                lineHeight: "28px",
+            },
+            regularX: {
+                fontFamily: 'IRANSansXRegular',
+                fontSize: 16,
+                lineHeight: "24px",
+            },
+            regular: {
+                fontFamily: 'IRANSansXRegular',
+                fontSize: 14,
+                lineHeight: "21px",
+            },
+            regularS: {
+                fontFamily: 'IRANSansXRegular',
+                fontSize: 12,
+                lineHeight: "18px",
+            }
         },
-        demiBold: {
-            fontFamily: 'IRANSansXDemiBold',
-            fontSize: 16,
-            lineHeight: "24px",
-        },
-        demiBoldS: {
-            fontFamily: 'IRANSansXDemiBold',
-            fontSize: 14,
-            lineHeight: "21px",
-        },
-        mediumS: {
-            fontFamily: 'IRANSansXMedium',
-            fontSize: 14,
-            lineHeight: "21px",
-        },
-        medium: {
-            fontFamily: 'IRANSansXMedium',
-            fontSize: 16,
-            lineHeight: "24px",
-        },
-        mediumX: {
-            fontFamily: 'IRANSansXMedium',
-            fontSize: 16,
-            lineHeight: "28px",
-        },
-        regularX: {
-            fontFamily: 'IRANSansXRegular',
-            fontSize: 16,
-            lineHeight: "24px",
-        },
-        regular: {
-            fontFamily: 'IRANSansXRegular',
-            fontSize: 14,
-            lineHeight: "21px",
-        },
-        regularS: {
-            fontFamily: 'IRANSansXRegular',
-            fontSize: 12,
-            lineHeight: "18px",
-        }
-    },
-    components: {
-        MuiLink: {
-            defaultProps: {
-                component: LinkBehavior,
+        components: {
+            MuiLink: {
+                defaultProps: {
+                    component: LinkBehavior,
+                },
+            },
+            MuiButtonBase: {
+                defaultProps: {
+                    LinkComponent: LinkBehavior,
+                },
             },
         },
-        MuiButtonBase: {
-            defaultProps: {
-                LinkComponent: LinkBehavior,
-            },
-        },
     },
-});
+);
 
 function App() {
     const dispatch = useDispatch()
@@ -146,19 +147,19 @@ function App() {
     }, [dispatch])
 
     return (
-      <CacheProvider value={cacheRtl}>
-          <ThemeProvider theme={theme}>
-              <BrowserRouter>
-                  <Header/>
-                  <Routes>
-                      <Route exact path={'/'} element={<MainPage />}/>
-                      <Route exact path='/call/:number' element={<PhoneCall />}/>
-                      <Route exact path='/redirect/' element={<Redirect />}/>
-                  </Routes>
-                  <Footer />
-              </BrowserRouter>
-          </ThemeProvider>
-      </CacheProvider>
+        <CacheProvider value={cacheRtl}>
+            <ThemeProvider theme={theme}>
+                <BrowserRouter>
+                    {/*<Header/>*/}
+                    <Routes>
+                        <Route exact path={'/'} element={<MainPage/>}/>
+                        <Route exact path='/call/:number' element={<PhoneCall/>}/>
+                        <Route exact path='/redirect/' element={<Redirect/>}/>
+                    </Routes>
+                    {/*<Footer />*/}
+                </BrowserRouter>
+            </ThemeProvider>
+        </CacheProvider>
     );
 }
 
