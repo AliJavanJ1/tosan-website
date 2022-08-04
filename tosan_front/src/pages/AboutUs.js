283903c4160e310db96fc14d3947cd82cc42d684
AJAliJavan <aj.ali.javan@gmail.com>
2022-08-04 17:04:05 +0430

    feat(front/Aboutus): add Aboutus page for test

---
+++ tosan_front/src/pages/AboutUs.js
@@ -0,0 +1,27 @@
+import {useState} from "react";
+import parse from "html-react-parser";
+import {CircularProgress, Stack} from "@mui/material";
+import {useSelector} from "react-redux";
+
+
+function AboutUs() {
+    const [content, setContent] = useState()
+    const serverURL = useSelector(store => store.static.wpDomain)
+
+    return (
+        content ? parse(content) : <Stack direction="column" alignItems="center" justifyContent="center" height="fit-content" width="100%" minHeight="100vh">
+            <iframe
+                name="report"
+                src={serverURL + "/contact-us"}
+                onLoad={iframe => setTimeout(() => setContent(iframe.target.contentDocument.documentElement.innerHTML), 1)}
+                style={{
+                    border: 0,
+                    height: 0,
+                    width: 0,
+            }}/>
+            <CircularProgress size={100} sx={{ color: "primary.shade3" }}/>
+        </Stack>
+    );
+}
+
+export default AboutUs;
