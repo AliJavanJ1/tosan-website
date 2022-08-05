9994d2d1c30f5b501ea4ac95b6d21ca381ebaf47
AJAliJavan <aj.ali.javan@gmail.com>
2022-08-05 18:01:44 +0430

    feat(front/AboutUs): show content in iframe with height update

--- tosan_front/src/pages/AboutUs.js
+++ tosan_front/src/pages/AboutUs.js
@@ -1,11 +1,10 @@
 import {useEffect, useState} from "react";
-import parse from "html-react-parser";
 import {CircularProgress, Stack} from "@mui/material";
 import {useSelector} from "react-redux";
 
 
 function AboutUs() {
-    const [content, setContent] = useState()
+    const [contentHeight, setContentHeight] = useState()
     const wpDomain = useSelector(store => store.static.wpDomain)
     const wpPageURL = wpDomain + "/contact-us"
 
@@ -16,9 +15,10 @@ function AboutUs() {
                 console.log("wrong_origin", event.origin)
                 return
             }
-            const fixedData = event.data.replaceAll("transition-duration: 0ms", "transition-duration: 1000ms").replaceAll("width: 0px", "width: 100%")
-            console.log("data", fixedData)
-            setContent(fixedData)
+            // const fixedData = event.data.replaceAll("transition-duration: 0ms", "transition-duration: 1000ms").replaceAll("width: 0px", "width: 100%")
+            const fixedData = event.data
+            console.log("data: ", fixedData, event.origin)
+            setContentHeight(fixedData)
         })
         console.log("after addEventListener")
     }, [])
@@ -31,37 +31,28 @@ function AboutUs() {
             justifyContent="center"
             height="fit-content"
             width="100%"
-            sx={{
-                "& html": {
-                    all: "initial",
-                },
-                "& html::before, html::after, html *::before, html *::after": {
-                    all: "unset",
-                },
-            }}
         >
-            {content ? parse(content) : <>
-                <iframe
-                    name="report"
-                    src={wpPageURL}
-                    onLoad={iframe => {
-                        console.log("before postMessage", iframe.target)
-                        iframe.target.contentWindow.postMessage('get html request', wpPageURL);
-                        console.log("after postMessage")
-                    }}
-                    style={{
-                        border: 0,
-                        height: 0,
-                        width: 0,
-                    }}/>
-                <CircularProgress
-                    size="10vw"
-                    sx={{
-                        color: "primary.shade3",
-                        marginY: "5vw",
-                    }}
-                />
-            </>}
+            <iframe
+                name="report"
+                src={wpPageURL}
+                onLoad={iframe => {
+                    console.log("before postMessage", iframe.target)
+                    iframe.target.contentWindow.postMessage('start onresize', wpPageURL);
+                    console.log("after postMessage")
+                }}
+                style={{
+                    border: 0,
+                    height: contentHeight || 0,
+                    width: contentHeight ? "100%" : 0,
+                }}
+            />
+            {!contentHeight && <CircularProgress
+                size="10vw"
+                sx={{
+                    color: "primary.shade3",
+                    marginY: "5vw",
+                }}
+            />}
         </Stack>
     );
 }
