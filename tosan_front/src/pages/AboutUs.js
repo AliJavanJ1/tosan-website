ff77a84250ae93a0ca0b40431752d01be9b6cd1b
AJAliJavan <aj.ali.javan@gmail.com>
2022-08-05 14:08:00 +0430

    feat(front/AboutUs): usePostMessage for getting html from wordpress

--- tosan_front/src/pages/AboutUs.js
+++ tosan_front/src/pages/AboutUs.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import parse from "html-react-parser";
 import {CircularProgress, Stack} from "@mui/material";
 import {useSelector} from "react-redux";
@@ -6,27 +6,52 @@ import {useSelector} from "react-redux";
 
 function AboutUs() {
     const [content, setContent] = useState()
-    const serverURL = useSelector(store => store.static.wpDomain)
+    const wpDomain = useSelector(store => store.static.wpDomain)
+    const wpPageURL = wpDomain + + "/contact-us"
+
+    useEffect(() => {
+        const iframe = document.getElementById("about-us-iframe")
+        iframe.postMessage('Request DOM manipulation', wpPageURL);
+
+        iframe.addEventListener('message', (event) => {
+            if (event.origin !== wpDomain) {
+                console.log("wrong_origin", event.origin)
+                return
+            }
+            console.log("data", event.data)
+            setContent(event.data)
+        })
+    }, [])
+
 
     return (
-        content ? parse(content) : <Stack direction="column" alignItems="center" justifyContent="center" height="fit-content" width="100%" minHeight="100vh">
-            <iframe
-                name="report"
-                src={serverURL + "/contact-us"}
-                onLoad={iframe => {
-                    console.log("0 kir: ", iframe.target)
-                    // setTimeout(() => console.log("3000 cos: ", iframe.target), 3000)
-                    console.log(iframe.target.contentDocument)
-                    console.log(iframe.target.contentDocument.documentElement)
-                    console.log(iframe.target.contentDocument.documentElement.innerHTML)
-                    setTimeout(() => setContent(iframe.target.contentDocument.documentElement.innerHTML), 1)
-                }}
-                style={{
-                    border: 0,
-                    height: 0,
-                    width: 0,
-            }}/>
-            <CircularProgress size={100} sx={{ color: "primary.shade3" }}/>
+        <Stack direction="column" alignItems="center" justifyContent="center" height="fit-content" width="100%">
+            {content ? parse(content) : <>
+                <iframe
+                    id="about-us-iframe"
+                    name="report"
+                    src={wpPageURL}
+                    // onLoad={iframe => {
+                    //     console.log("0 kir: ", iframe.target)
+                    //     // setTimeout(() => console.log("3000 cos: ", iframe.target), 3000)
+                    //     console.log(iframe.target.contentDocument)
+                    //     console.log(iframe.target.contentDocument.documentElement)
+                    //     console.log(iframe.target.contentDocument.documentElement.innerHTML)
+                    //     setTimeout(() => setContent(iframe.target.contentDocument.documentElement.innerHTML), 1)
+                    // }}
+                    style={{
+                        border: 0,
+                        height: 0,
+                        width: 0,
+                    }}/>
+                <CircularProgress
+                    size="30vw"
+                    sx={{
+                        color: "primary.shade3",
+                        marginY: "15vw",
+                    }}
+                />
+            </>}
         </Stack>
     );
 }
