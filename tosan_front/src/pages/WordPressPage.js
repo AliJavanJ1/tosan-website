875aeb35c30eb82938e9941fa3abc04cbd7a0b65
AliJavanJ1 <aj.ali.javan@gmail.com>
2022-08-19 21:35:07 +0430

    feat(front/WordPressPage): add beforeunload event listener to iframe

--- tosan_front/src/pages/WordPressPage.js
+++ tosan_front/src/pages/WordPressPage.js
@@ -1,12 +1,15 @@
 import {useEffect, useState} from "react";
 import {useSelector} from "react-redux";
 import {CircularProgress, Stack} from "@mui/material";
+import { useNavigate } from 'react-router-dom';
 
-function WordPressPage({wpPath, preHeight}) {
+
+function WordPressPage({wpPath, preHeight, sx}) {
     const [contentHeight, setContentHeight] = useState()
     const wpDomain = useSelector(store => store.static.wpDomain)
     const wpPageURL = wpDomain + wpPath
     const preContentHeights = [preHeight || "100vh", `calc(${preHeight || "100vh"} - 1px + 1px)`]
+    const navigate = useNavigate()
 
     useEffect(() => {
         // console.log("before addEventListener")
@@ -45,6 +48,7 @@ function WordPressPage({wpPath, preHeight}) {
             height={contentHeight ? "fit-content" : preContentHeights[0]}
             width="100%"
             position="relative"
+            sx={sx}
         >
             <iframe
                 id="content-iframe"
@@ -56,9 +60,14 @@ function WordPressPage({wpPath, preHeight}) {
                     console.log("before postMessage", iframe)
                     iframe.contentWindow.postMessage('start onresize', wpDomain)
                     console.log("after postMessage")
+                    iframe.contentWindow.addEventListener("beforeunload", (event) => {
+                        console.log(event)
+                        event.preventDefault()
+                        navigate('/about-us')
+                    })
                 }}
                 scrolling="no"
-                sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-presentation allow-same-origin allow-scripts"
+                sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox	allow-presentation allow-same-origin allow-scripts allow-top-navigation"
                 style={{
                     border: 0,
                     height: contentHeight || 0,
